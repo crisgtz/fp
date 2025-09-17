ac420d1c62f80ed69c3599578501654ea643af56
crisgtz <57165050+crisgtz@users.noreply.github.com>
2025-09-17 07:34:58 -0600

    unidad 2 ejersicios

---
+++ unidad 2/ejersicio.py
@@ -0,0 +1,12 @@
+#ESTE PROGRAMAIMPRIME LA VARIABLE. TAMBIEN OBSERVAMOS LA MUTABILIDAD DE PYTHON#
+entero = 10
+flotante = 3.14
+cadena = "hdp"
+booleana = True
+
+print(entero)
+print(flotante)
+print(cadena)
+
+"cris"
+"ITPA"
