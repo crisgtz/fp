b108c71fc785eaab4e2efbdf75c70fa77baaefb0
crisgtz <57165050+crisgtz@users.noreply.github.com>
2025-09-18 07:59:05 -0600

    lista

---
+++ unidad 2/lista.py
@@ -0,0 +1,20 @@
+lista = ["Juan","Ivan", "Adrian", "karlita"]
+print (lista)
+
+print (lista[2])
+
+lista.append("Alexa")
+print (lista)
+#esta funcion es para agregar una al final de la lista 
+
+lista.insert (1,"Javier")
+print (lista)
+#sirve para agregar un valor a la lista 
+
+lista.remove("Juan")
+print(lista)
+#esat funcion sirve para eliminar un valor en espesifico pero solo por el nombre del valor 
+
+lista.pop(1)
+print(lista)
+#sirve para eliminar un valor en la lista por susecion numerica  
